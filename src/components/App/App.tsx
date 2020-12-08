9710fed37d8e31fd834c526dab44f4c14f15d8dd
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2020-12-08 15:00:55 -0300

    Refatorando componentes App e Header (Material-UI)

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,50 @@
+import { Container, withStyles } from '@material-ui/core';
 import React from 'react';
+import { BrowserRouter, Switch } from 'react-router-dom';
 import Header from '../Header';
-import './App.css';
+import { HeaderMenu } from '../Header';
 
-function App() {
+const styles = {
+	containerContent: {
+        backgroundColor: '#ecf0f5',
+        minHeight: '93vh',
+	}
+};
 
-    return (
-        <div className="App">
-            <Header title="AlgaStock" />
-        </div>
-    );
+declare interface AppProps {
+    classes?: {
+        [seletor: string]: string
+    }
 }
 
-export default App;
+const headersMenu: Array<HeaderMenu> = [
+	{
+		nome: 'Banco',
+		link: '/'
+	},
+	{
+		nome: 'Contas',
+		link: '/'
+	},
+	{
+		nome: 'Sobre',
+		link: '/'
+	}
+];
+
+const App: React.FC<AppProps> = (props) => {
+    return (
+        <div className="AppComponent">
+            <BrowserRouter>
+                <Header headersMenu={headersMenu} />
+                <Container className={props.classes?.containerContent}>
+                    <Switch>
+
+                    </Switch>
+                </Container>
+            </BrowserRouter>
+        </div>
+    )
+}
+
+export default withStyles(styles)(App);
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,70 @@
 import React from 'react';
-import './Header.css'
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import { NavLink } from 'react-router-dom'
+import Container from '@material-ui/core/Container';
+import { withStyles } from '@material-ui/core';
+
+const styles = {
+	header: {
+		backgroundColor: '#dd4b39',
+		height: '50px',
+		display: 'flex',
+		alignItems: 'center',
+		alignContent: 'center',
+		padding: '10px 15px',
+		boxShadow: '0 3px 6px rgba(0, 0, 0, .2)',
+		marginBottom: '10px'
+	},
+	'@global': {
+		'li:nth-of-type(1)': {
+			fontWeight: '900',
+		}
+	},
+	linkContent: {
+		color: '#FFF',
+		textDecoration: 'none',
+		padding: '10px',
+		fontSize: '16px',
+		'&:hover': {
+			textDecoration: 'none',
+			backgroundColor: 'rgba(0, 0, 0, .1)',
+			borderRadius: '10px',
+		}
+	}
+};
+
+export interface HeaderMenu {
+	nome: string,
+	link: string
+};
 
 declare interface HeaderProps {
-
-    title: string
+	classes?: {
+		[seletor: string]: string
+	},
+	headersMenu: Array<HeaderMenu>
 }
 
 const Header: React.FC<HeaderProps> = (props) => {
-
-    return <header className="AppHeader">
-        <h1>{ props.title }</h1>
-    </header>
+	return <header className={props.classes?.header}>
+		<Container>
+			<Breadcrumbs arial-label="breadcrumb">
+				{
+					props.headersMenu.map(header => {
+						return (
+							<NavLink 
+								className={props.classes?.linkContent}
+								key={header.nome}
+								to={header.link}
+							>
+								{ header.nome }
+							</NavLink>
+						)
+					})
+				}
+			</Breadcrumbs>
+		</Container>
+	</header>
 }
 
-export default Header;
+export default withStyles(styles)(Header);
